3ac68007c866a4bc4e2af0e1f8d3aaffda724cc2
luoa <luoa@uci.edu>
2009-09-03 14:33:13 -0700

    Cleaned up twitterajax code, added stopwatch

--- public/teliosdev/javascripts/twitterajax.js
+++ public/teliosdev/javascripts/twitterajax.js
@@ -1,39 +1,30 @@
-var tweet = new Array();
-var newtweet = new Array();
-var tweetbox;
 
 function buildTweets() {
-	alert('gah!');
-	$.getJSON("http://twitter.com/statuses/user_timeline/teliosdev.json?count=10&format=json&callback=?", function(data) {
-		alert('hmm');
-		for(var i = 0; i < data.length; i = i + 1) {
-			tweet[0] = data[i].created_at + ' ' + data[i].text;
-			alert('hi');
-			if (i === 0 || i%2 === 0) {
-				tweetbox = "<div id='tweetbox'" + i + " class='tweetbox0'>" + tweet[0] + "</div>";
-				$('#twitter').append(tweetbox);
-				alert('1:build!');
-			} else {
-				tweetbox = "<div id='tweetbox'" + i + " class='tweetbox1'>" + tweet[0] + "</div>";
-				$('#twitter').append(tweetbox);
-				alert('2:build!');
-			}
+	$.getJSON("http://teliosdev:hboxrocks@twitter.com/statuses/user_timeline/teliosdev.json?count=10&format=json&callback=?", function(data, textStatus) {
+		$('#tweets').html("");
+		for(i = 0; i < data.length; i = i + 1) {
+			tweet = data[i].text;
+			tweet_box = document.createElement('div');
+			tweet_box.setAttribute('id', 'tweet_' + i);
+			tweet_box.setAttribute('class', 'tweet_type_' + i % 2);
+			tweet_box.innerHTML = tweet;
+			$('#tweets').append(tweet_box);
 		}
 	});
 };
 
-function updateTweets() {
-	alert('updated!');
-	$.getJSON("http://twitter.com/statuses/user_timeline/teliosdev.json?count=10&format=json&callback=?", function(data) {
-		alert('hmm');
+
+/*
+function updateTweets1() {
+	$.getJSON("http://twitter.com/statuses/user_timeline/allenluo.json?count=10&format=json&callback=?", function(data) {
 		for (var j = 0; j < data.length; j = j + 1) {
 			newtweet[0] = data[i].created_at + ' ' + data[i].text;
-			console.log(tweet[i]);
-			console.log(newtweet[i]);
+			console.log(tweet);
+			console.log(newtweet);
 			if (tweet[i] !== newtweet[i]) {
 				$('#tweetbox' + i).html(newtweet[i]);
 			}
 		}
 	});
 };
-	
+*/	
